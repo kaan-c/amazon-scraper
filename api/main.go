96f38d4b717a4104c37515eee45cc068d443069a
Kaan Ceylan <kaanyceylan@gmail.com>
2021-12-04 02:29:04 +0300

    Endpoint GET service is converted to POST service and modified to accept custom Amazon addresses. Cronjob is removed temporarily.

--- api/main.go
+++ api/main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/gocolly/colly"
 	"github.com/gorilla/mux"
-	"github.com/robfig/cron"
 )
 
 type Product struct {
@@ -21,73 +17,55 @@ type Product struct {
 	Price float64 `json:"price"`
 }
 
+type Address struct {
+	AddressData string `json:"address"`
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-	vars := mux.Vars(r)
-	address := vars["address"]
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var addr Address
+	_ = json.NewDecoder(r.Body).Decode(&addr)
+	address := addr.AddressData
 	log.Println("address is:" + address)
 
 	allProducts := make([]Product, 0)
 
-	cronJob := cron.New()
-	cronJob.AddFunc("@every 8s", func() {
-		collector := colly.NewCollector(
-			colly.AllowURLRevisit(),
-		)
+	collector := colly.NewCollector(
+		colly.AllowURLRevisit(),
+	)
 
-		collector.Limit(&colly.LimitRule{
-			DomainGlob:  "https://www.amazon.com.tr/*",
-			Delay:       3 * time.Second,
-			RandomDelay: 3 * time.Second,
-		})
-
-		collector.OnHTML(".octopus-dlp-asin-section", func(element *colly.HTMLElement) {
-			title := element.ChildAttr(".a-size-base", "title")
-			priceStr := "" + element.ChildText(".a-price-whole") + element.ChildText(".a-price-fraction")
-			priceStr = strings.ReplaceAll(priceStr, ",", "")
-			price, err := strconv.ParseFloat(priceStr, 64)
-			if err != nil {
-				price = 0
-			}
-			product := Product{
-				Title: title,
-				Price: price,
-			}
-			allProducts = append(allProducts, product)
-		})
-
-		collector.OnRequest(func(request *colly.Request) {
-			fmt.Println("Visiting", request.URL.String())
-		})
-
-		enc := json.NewEncoder(os.Stdout)
-		enc.SetIndent("", " ")
-		enc.Encode(allProducts)
-
-		writeJSON(allProducts)
-		collector.Visit("https://www.amazon.com.tr/gp/bestsellers/?ref_=nav_em_cs_bestsellers_0_1_1_2" + address)
+	collector.OnHTML(".octopus-dlp-asin-section", func(element *colly.HTMLElement) {
+		title := element.ChildAttr(".a-size-base", "title")
+		priceStr := "" + element.ChildText(".a-price-whole") + element.ChildText(".a-price-fraction")
+		priceStr = strings.ReplaceAll(priceStr, ",", "")
+		price, err := strconv.ParseFloat(priceStr, 64)
+		if err != nil {
+			price = 0
+		}
+		product := Product{
+			Title: title,
+			Price: price,
+		}
+		allProducts = append(allProducts, product)
 	})
-	cronJob.Start()
-	time.Sleep(time.Second * 100)
+
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL.String())
+	})
+	collector.Visit(address)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(allProducts)
 }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/address/{address}", homePage)
+	myRouter.HandleFunc("/address", homePage).Methods("POST", "OPTIONS")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
 	handleRequests()
-}
-
-func writeJSON(data []Product) {
-	file, err := json.MarshalIndent(data, "", " ")
-	if err != nil {
-		log.Println("Unable to create JSON file")
-		return
-	}
-
-	_ = ioutil.WriteFile("data.json", file, 0644)
 }
--- frontend/options-page/src/App.js
+++ frontend/options-page/src/App.js
@@ -5,15 +5,20 @@ import React from 'react';
 class pageComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {address: ""}; 
+    this.state = {address: "", dataJson: ""};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this); 
   }
   handleSubmit(event) {
     console.log("address is " + this.state.address);
-    fetch("http://localhost:10000/address/" + this.state.address)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ address: this.state.address })
+    };
+    fetch("http://localhost:10000/address", requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
+      .then((data) => this.setState({address:"", dataJson: data[0].title}));
     event.preventDefault();
   }
 
@@ -22,11 +27,17 @@ class pageComponent extends React.Component {
   }
 
   render() {
-    return (<form onSubmit={this.handleSubmit}>
-          <label>Address:</label>
-          <input type="text" id="url" value={this.state.address} onChange={this.handleChange} />
-          <button type="submit">Submit</button>
-    </form>);
+    return (
+    <html>
+      <form onSubmit={this.handleSubmit}>
+            <label>Address:</label>
+            <input type="text" id="url" value={this.state.address} onChange={this.handleChange} />
+            <br />
+            <button type="submit">Submit</button>
+            <br/>
+            <label>{this.state.dataJson}</label>
+      </form>
+    </html>);
   }
 }
 
